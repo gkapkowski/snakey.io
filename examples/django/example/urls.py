ce282b2dcfd3197087d9e47f1d832517b506e6f6
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2017-07-18 19:41:08 +0200

    New start

--- examples/django/example/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^snake/$', 'example.views.main_snake_view', name='main'),
-    url(r'^random/snake/$', 'example.views.random_snake_view', name='random'),
-    url(r'^random/tank/$', 'example.views.random_tank_view', name='random'),
-)
--- src/client/app/state.js
+++
@@ -1,7 +0,0 @@
-/* global module, require */
-'use strict';
-var Backbone = require('backbone');
-
-var State = Backbone.Model.extend();
-
-module.exports = State;
